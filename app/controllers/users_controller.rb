abc6f7021f40f284a4502f93df02e4475f50788f
Antonino <antoninoscaffidi@gmail.com>
2018-11-28 09:54:48 +0100

    Added action show to users resouces

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:  [:show, :edit, :update, :detroy]
+  
+  def index
+  end
+  
+  def show
+  end
+
   def new
   end
+  
+  def edit
+  end
+
+  def create
+  end
+  
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
